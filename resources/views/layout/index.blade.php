ef3452e6898e42f1a22eb63a67da9ac7685a7cdc
Jesus Moreno <jesus.moreno@janaq.com>
2019-11-12 18:35:17 -0500

    refactor(layout): modificación de layout administracion

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -9,103 +9,87 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
-   
-    <div class="row">
-        <div class="col-12">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">Qhatu</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarColor01">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                </ul>
-                </div>
-            </nav>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Qhatu</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+            </ul>
         </div>
-    </div>
+    </nav>
+    <!-- navbar -->
     <div class="clearfix">&nbsp;</div>
-    <div class="container-fluid">
+    <!-- container -->
+    <div class="container">
         <div class="row">
-            <div class="col-2 bg-light">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Active</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-10">
-                
+            <div class="col-12">
                 <!-- Form -->
-                <form class="col-6">
-                <div class="form-group">
-                    <label for="">Example label</label>
-                    <input type="text" class="form-control" id="" placeholder="Example input">
-                </div>
-                <div class="form-group">
-                    <label for="">Another label</label>
-                    <input type="text" class="form-control" id="" placeholder="Another input">
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <form>
+                    <div class="form-group">
+                        <label for="">Example label</label>
+                        <input type="text" class="form-control" id="" placeholder="Example input">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Another label</label>
+                        <input type="text" class="form-control" id="" placeholder="Another input">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
-                <div class="clearfix">&nbsp;</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
                 <!-- Table -->
                 <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
-                </tbody>
+                    <thead class="thead-dark">
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
                 </table>
+                <!-- Table -->
             </div>
         </div>
     </div>
-
+    <!-- container -->
 <script src="/js/app.js"></script>
 <script>
     $.ajaxSetup({
